391b70626cb66bbd09ea00a17b7866c1ab3f83fe
nayrojas <nayllen.rojas@gmail.com>
2025-09-01 16:57:32 -0500

    feat: new header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GlobalHeaderComponent } from './componentes/global-header.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GlobalHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
